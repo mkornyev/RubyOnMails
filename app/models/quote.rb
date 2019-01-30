b71fb983b0467efcc35e92b1bdd9c3cf1095ab7b
Max Kornyev <mkornyev@gmail.com>
2019-01-29 20:17:11 -0500

    Basic MVC Architecture added

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,13 @@
 class Quote < ApplicationRecord
+	
+	# Method to get a random quote for home page
+	# 
+	# @return [Quote]
+	#   the random quote 
+	
+	validates_presence_of :body
+
+	def self.get_random_quote
+		self.all.sample
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
 	resources :quotes
 
 	get 'home' => 'home#index', as: :home
